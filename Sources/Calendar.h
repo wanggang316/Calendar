ed920c755616b5263259408e9f61299e7b343a90
Gump <1989wg@gmail.com>
2016-11-15 13:11:01 +0800

    adjust file structure

---
+++ Sources/Calendar.h
@@ -0,0 +1,19 @@
+//
+//  Calendar.h
+//  Calendar
+//
+//  Created by wanggang on 15/11/2016.
+//  Copyright © 2016 GUM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Calendar.
+FOUNDATION_EXPORT double CalendarVersionNumber;
+
+//! Project version string for Calendar.
+FOUNDATION_EXPORT const unsigned char CalendarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Calendar/PublicHeader.h>
+
+
