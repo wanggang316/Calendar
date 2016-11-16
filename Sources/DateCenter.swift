7a22dc6ab057a56e4ed558e2280723ee54b737d3
Gump <1989wg@gmail.com>
2016-11-16 19:53:11 +0800

    add DateCenter

---
+++ Sources/DateCenter.swift
@@ -0,0 +1,38 @@
+
+//
+//  DateCenter.swift
+//  Calendar
+//
+//  Created by wanggang on 16/11/2016.
+//  Copyright © 2016 GUM. All rights reserved.
+//
+
+import Foundation
+
+/**
+ Date tool box.
+ */
+extension Date {
+    
+    
+    
+}
+
+
+/**
+ extension for IndexPath
+ */
+extension NSDate {
+    
+    class func date(forFirstDayInSection day: Date, firstDate: Date) -> Date? {
+        return nil
+    }
+    
+    class func date(at indexPath: IndexPath, firstDate: Date) -> Date? {
+        return nil
+    }
+    
+    class func indexPath(forDate date: Date, firstDate: Date) -> IndexPath? {
+        return nil
+    }
+}
