184d194dba16890d48fd0bb232ee9266be8dc8a7
Gump <1989wg@gmail.com>
2016-11-17 11:35:30 +0800

    add convince function for get date element

--- Sources/DateCenter.swift
+++ Sources/DateCenter.swift
@@ -12,9 +12,86 @@ import Foundation
 /**
  Date tool box.
  */
-extension Date {
+public extension Date {
     
+    public var era: Int {
+        let calendar = Calendar.current
+        return calendar.component(.era, from: self)
+    }
     
+    public var year: Int {
+        let calendar = Calendar.current
+        return calendar.component(.year, from: self)
+    }
+    
+    public var month: Int {
+        let calendar = Calendar.current
+        return calendar.component(.month, from: self)
+    }
+    public var day: Int {
+        let calendar = Calendar.current
+        return calendar.component(.day, from: self)
+    }
+    
+    public var weekday: Int {
+        let calendar = Calendar.current
+        return calendar.component(.weekday, from: self)
+    }
+    
+    public var hour: Int {
+        let calendar = Calendar.current
+        return calendar.component(.hour, from: self)
+    }
+    
+    public var minute: Int {
+        let calendar = Calendar.current
+        return calendar.component(.minute, from: self)
+    }
+    
+    public var second: Int {
+        let calendar = Calendar.current
+        return calendar.component(.second, from: self)
+    }
+    
+    public var weekdayOrdinal: Int {
+        let calendar = Calendar.current
+        return calendar.component(.weekdayOrdinal, from: self)
+    }
+    
+    public var quarter: Int {
+        let calendar = Calendar.current
+        return calendar.component(.quarter, from: self)
+    }
+    
+    public var weekOfMonth: Int {
+        let calendar = Calendar.current
+        return calendar.component(.weekOfMonth, from: self)
+    }
+    
+    public var weekOfYear: Int {
+        let calendar = Calendar.current
+        return calendar.component(.weekOfYear, from: self)
+    }
+    
+    public var yearForWeekOfYear: Int {
+        let calendar = Calendar.current
+        return calendar.component(.yearForWeekOfYear, from: self)
+    }
+    
+    public var nanosecond: Int {
+        let calendar = Calendar.current
+        return calendar.component(.nanosecond, from: self)
+    }
+    
+    public var calendar: Int {
+        let calendar = Calendar.current
+        return calendar.component(.calendar, from: self)
+    }
+    
+    public var timeZone: Int {
+        let calendar = Calendar.current
+        return calendar.component(.timeZone, from: self)
+    }
     
 }
 
