2ed0b56df0de4cd0ff14458ee1c8bdd309e7396d
Gump <1989wg@gmail.com>
2016-12-30 11:07:44 +0800

    rafactor index list

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -8,34 +8,54 @@
 
 import UIKit
 
+enum TableRow: String {
+    case showView = "view"
+    case singleSelectionGeneral = "general"
+    case rangeSelectionSimple = "simple"
+    case rangeSelectionComplex = "complex"
+}
+
+struct TableSection {
+    let title: String
+    let cells: [TableRow]
+}
+
 class ViewController: UITableViewController {
+    
+    let tableData = [TableSection(title: "Show", cells: [TableRow.showView]),
+                    TableSection(title: "Single Selection", cells: [TableRow.singleSelectionGeneral]),
+                    TableSection(title: "Range Selection", cells: [TableRow.rangeSelectionSimple, TableRow.rangeSelectionComplex])]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.title = "Calendar"
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
 }
 
 extension ViewController {
     
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return tableData.count
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return tableData[section].cells.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         
+        let cellData = tableData[indexPath.section].cells[indexPath.row]
+        cell.textLabel?.text = cellData.rawValue
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return tableData[section].title
     }
 }
 
@@ -43,12 +63,18 @@ extension ViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
         
-        if indexPath.row == 0 {
+        let cellData = tableData[indexPath.section].cells[indexPath.row]
+        
+        switch cellData {
+        case .showView:
+            break
+        case .singleSelectionGeneral:
             let controller = SimpleViewController()
             self.navigationController?.pushViewController(controller, animated: true)
-        } else {
-            let controller = TableViewController()
-            self.navigationController?.pushViewController(controller, animated: true)
+        case .rangeSelectionSimple:
+            break
+        case .rangeSelectionComplex:
+            break
         }
     }
 }
